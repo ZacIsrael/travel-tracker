ca5aa2274d83af3d4f76d6bc71fac5d9b62299d7
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-04-24 14:00:44 -0400

    Using constants and template literals for queries instead of manually typing table names in db.query() methods, less likely for human error.

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const port = 3000;
 // name of the table in the postgreSQL database that stores the codes of the visited countries
 const visitedCountriesTable = "visited_countries";
 
+// name of the table in the postgreSQL database that stores all the countries & their respective codes
+const countries = "countries";
+
 // Set EJS as the view engine
 app.set("view engine", "ejs");
 
@@ -45,7 +48,7 @@ app.get("/", async (req, res) => {
     // array that will get sent to the EJS file
     let countries = [];
     // retrieve all of the rows/entries from the "visited_countries" table in the "world" database
-    let queryResult = await db.query("SELECT * FROM visited_countries");
+    let queryResult = await db.query(`SELECT * FROM ${visitedCountriesTable}`);
 
     visitedCountries = queryResult.rows;
 
@@ -72,20 +75,21 @@ app.get("/", async (req, res) => {
 // allows a user to add a country (code) that they have visited
 app.post("/add", async (req, res) => {
   console.log("req.body, ", req.body);
-  let countryToAddCode = req.body.country;
-  // ensures that the code is in ALL caps because that's how they are stored in the visited_countries table
-  countryToAddCode = countryToAddCode.toUpperCase();
+  let countryToAdd = req.body.country;
+  
+  if(countryToAdd.trim().length === 0){
+    // user put space(s) as the country to be added
+    console.error(`POSTT \'/add\' route: Please enter a country before clicking the \'add\' button.`);
 
-  if (countryToAddCode.length !== 2) {
-    console.error(
-      `POST \'/add\' route: Country code must be 2 characters long. ${countryToAddCode} is NOT 2 characters long.`
-    );
   } else {
-    let addedCountry = await db.query(
-      `INSERT INTO ${visitedCountriesTable} (country_code) VALUES (\'${countryToAddCode})\'`
-    );
-    console.log("addedCountry query result = ", addedCountry);
+    // Check countries table to see if the user's input is actually a valid country
+
+    // If a country is returned, get its country code and then add it to the visited_countries
+    // table. Otherwise, throw an error saying that the user's input is not a valid country
+
   }
+
+  
 });
 
 // close the connection to the database
